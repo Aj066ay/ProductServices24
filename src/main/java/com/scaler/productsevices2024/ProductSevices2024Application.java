0f78aa53c861a499795291ddafa822472914bdab
ajayphani <jay066royal@gmail.com>
2024-01-11 08:55:06 +0530

    "Initial commit"

---
+++ src/main/java/com/scaler/productsevices2024/ProductSevices2024Application.java
@@ -0,0 +1,13 @@
+package com.scaler.productsevices2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSevices2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSevices2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/productsevices2024/ProductSevices2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.productsevices2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSevices2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
